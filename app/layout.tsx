fda570956d7af5263f2a609a2b07b5fa681cd271
Abhay Kumar <abhay292005@gmail.com>
2024-12-30 22:01:52 +0530

    image added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,98 +1,98 @@
-import "./globals.css";
-import { ReactNode } from "react";
-import type { Metadata } from "next";
-import { Syne } from "next/font/google";
-import { Analytics } from "@vercel/analytics/react";
+  import "./globals.css";
+  import { ReactNode } from "react";
+  import type { Metadata } from "next";
+  import { Syne } from "next/font/google";
+  import { Analytics } from "@vercel/analytics/react";
 
-const syne = Syne({
-  subsets: ["latin"],
-  display: "block",
-  weight: ["400", "500", "600", "700", "800"],
-});
+  const syne = Syne({
+    subsets: ["latin"],
+    display: "block",
+    weight: ["400", "500", "600", "700", "800"],
+  });
 
-export const metadata: Metadata = {
-  title: "Abhay Kumar — Web developer",
-  description:
-    "Frontend Engineer and Web Designer, currently at Kora. Focused on interfaces and experiences, working remotely from Lagos, Nigeria.",
-  generator: "Next.js",
-  applicationName: "Abhay Kumar",
-  keywords: [
-    "freelance",
-    "developer",
-    "freelance developer",
-    "frontend",
-    "react",
-    "frontend developer",
-    "frontend engineer",
-    "creative",
-    "creative developer",
-    "creative engineer",
-    "tech",
-    "India",
-    "software",
-    "software developer",
-    "portfolio",
-    "frontend developer portfolio",
-    "creative developer portfolio",
-  ],
-  colorScheme: "dark",
-  openGraph: {
+  export const metadata: Metadata = {
     title: "Abhay Kumar — Web developer",
     description:
       "Frontend Engineer and Web Designer, currently at Kora. Focused on interfaces and experiences, working remotely from Lagos, Nigeria.",
-    url: "https://abhaykumar.vercel.app/",
-    siteName: "abhaykumar.vercel.app",
-    images: [
-      {
-        url: "https://github.com/user-attachments/assets/605fa165-b8a3-4e79-9efc-a84dc18020fc",
-        width: 1200,
-        height: 630,
-        alt: "Abhay Kumar — Web Developer",
-      },
+    generator: "Next.js",
+    applicationName: "Abhay Kumar",
+    keywords: [
+      "freelance",
+      "developer",
+      "freelance developer",
+      "frontend",
+      "react",
+      "frontend developer",
+      "frontend engineer",
+      "creative",
+      "creative developer",
+      "creative engineer",
+      "tech",
+      "India",
+      "software",
+      "software developer",
+      "portfolio",
+      "frontend developer portfolio",
+      "creative developer portfolio",
     ],
-    locale: "en-US",
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Victor Williams — Frontend Engineer",
-    description:
-      "Frontend Engineer and Web Designer, currently at Kora. Focused on interfaces and experiences, working remotely from Lagos, Nigeria.",
-    creator: "victorwill__",
-    creatorId: "1243720976552144897",
-    images: [
-      "https://github.com/user-attachments/assets/605fa165-b8a3-4e79-9efc-a84dc18020fc",
-    ],
-  },
-  robots: {
-    index: true,
-    follow: true,
-    nocache: false,
-    googleBot: {
-      index: true,
-      follow: false,
-      noimageindex: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
+    colorScheme: "dark",
+    openGraph: {
+      title: "Abhay Kumar — Web developer",
+      description:
+        "Frontend Engineer and Web Designer, currently at Kora. Focused on interfaces and experiences, working remotely from Lagos, Nigeria.",
+      url: "https://abhaykumar.vercel.app/",
+      siteName: "abhaykumar.vercel.app",
+      images: [
+        {
+          url: "https://github.com/user-attachments/assets/605fa165-b8a3-4e79-9efc-a84dc18020fc",
+          width: 1200,
+          height: 630,
+          alt: "Abhay Kumar — Web Developer",
+        },
+      ],
+      locale: "en-US",
+      type: "website",
     },
-  },
-  category: "technology",
-};
+    twitter: {
+      card: "summary_large_image",
+      title: "Victor Williams — Frontend Engineer",
+      description:
+        "Frontend Engineer and Web Designer, currently at Kora. Focused on interfaces and experiences, working remotely from Lagos, Nigeria.",
+      creator: "victorwill__",
+      creatorId: "1243720976552144897",
+      images: [
+        "https://github.com/user-attachments/assets/605fa165-b8a3-4e79-9efc-a84dc18020fc",
+      ],
+    },
+    robots: {
+      index: true,
+      follow: true,
+      nocache: false,
+      googleBot: {
+        index: true,
+        follow: false,
+        noimageindex: true,
+        "max-video-preview": -1,
+        "max-image-preview": "large",
+        "max-snippet": -1,
+      },
+    },
+    category: "technology",
+  };
 
-type RootLayoutProps = {
-  children: ReactNode;
-};
+  type RootLayoutProps = {
+    children: ReactNode;
+  };
 
-export default function RootLayout({ children }: RootLayoutProps) {
-  return (
-    <html lang="en">
-      <body
-        className={`${syne.className} scroll-smooth scrollbar-thin scrollbar-track-[#0E1016] scrollbar-thumb-[#212531]`}
-      >
-        {children}
-        <Analytics />
-      </body>
-    </html>
-  );
-}
+  export default function RootLayout({ children }: RootLayoutProps) {
+    return (
+      <html lang="en">
+        <body
+          className={`${syne.className} scroll-smooth scrollbar-thin scrollbar-track-[#0E1016] scrollbar-thumb-[#212531]`}
+        >
+          {children}
+          <Analytics />
+        </body>
+      </html>
+    );
+  }
